d02605fd8110b4bbd3e10ed017fbd73dc4888630
Muhammadyunusxon <muhammadyunusxonabdumannopov9@gmail.com>
2024-10-19 14:44:34 +0500

    - clear code

--- lib/payfast.dart
+++ lib/payfast.dart
@@ -4,6 +4,6 @@ library payfast;
 
 export 'src/payfast_base.dart';
 export 'src/enums/enums.dart';
-
+export 'src/models/merchant_details.dart';
 
 // TODO: Export any libraries intended for clients of this package.
